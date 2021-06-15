64bfe1ff55b04ae7bb2b9a3cb53163fd0d25ceff
Amine Chograni <aminechograni@gmail.com>
2021-06-15 10:48:16 +0100

    Add project files.

---
+++ CatalogService/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using CatalogService.Modules;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogService
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
